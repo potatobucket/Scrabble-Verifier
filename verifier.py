9344f3d98603f1df6e2aa5009198a5087f2437a4
potatobucket <104957727+potatobucket@users.noreply.github.com>
2025-01-05 09:50:48 -0600

    Can query the API now
    It works!

--- verifier.py
+++ verifier.py
@@ -1,10 +1,17 @@
 from dotenv import load_dotenv
+import json
 import os
 import requests
+
+from pprint import pprint
 
 load_dotenv()
 
 apiKey = os.getenv("apiKey")
+urlBase = "https://api.wordnik.com/v4/word.json/"
 
 if __name__ == "__main__":
-    pass
+    scrabbleWord = input("Which word are you checking? ")
+    scoreGet = requests.get(f"{urlBase}/{scrabbleWord}/scrabbleScore?api_key={apiKey}").text
+    scoreGet = json.loads(scoreGet)
+    pprint(scoreGet["value"])
