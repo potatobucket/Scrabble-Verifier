e5b62b061cb0fb3a57eaba814efa19b2ed7b3034
potatobucket <104957727+potatobucket@users.noreply.github.com>
2025-01-06 00:05:06 -0600

    Errors
    Errors

---
+++ errors.py
@@ -0,0 +1,2 @@
+class WordNotFound(Exception):
+    pass
--- verifier.py
+++ verifier.py
@@ -1,17 +1,22 @@
 from dotenv import load_dotenv
+import errors
 import json
 import os
 import requests
-
-from pprint import pprint
 
 load_dotenv()
 
 apiKey = os.getenv("apiKey")
 urlBase = "https://api.wordnik.com/v4/word.json/"
 
+def get_scrabble_score(word):
+    requestGet = requests.get(f"{urlBase}/{word}/scrabbleScore?api_key={apiKey}")
+    if requestGet.status_code != 200:
+        raise errors.WordNotFound(f"{word} is not a valid word in this dictionary!")
+    else:
+        scoreGet = json.loads(requestGet.text)
+        return scoreGet["value"]
+
 if __name__ == "__main__":
-    scrabbleWord = input("Which word are you checking? ")
-    scoreGet = requests.get(f"{urlBase}/{scrabbleWord}/scrabbleScore?api_key={apiKey}").text
-    scoreGet = json.loads(scoreGet)
-    pprint(scoreGet["value"])
+    scrabbleWord = get_scrabble_score(input("Which word are you checking? "))
+    print(scrabbleWord)
