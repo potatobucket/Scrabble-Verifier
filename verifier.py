00c59a3d9a981de8d90add19403554fbfdcec779
potatobucket <104957727+potatobucket@users.noreply.github.com>
2025-01-07 15:36:42 -0600

    Cleanup
    Cleanup! Cleanup! Everybody, everywhere!

--- verifier.py
+++ verifier.py
@@ -1,10 +1,5 @@
 import letter_data as ld
 
 if __name__ == "__main__":
-    #scrabbleWord = get_scrabble_score(input("Which word are you checking? "))
-    #print(scrabbleWord)
-
     scrabbleWord = ld.word_value(input("Which word are you checking? "))
     print(scrabbleWord)
-
-    #print(ld.remove_blanks("quizzical"))
